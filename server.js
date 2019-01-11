aafd6144b4e943f29aa867cbc16cdbcf2e413f63
unknown <tiago.aro@sp01.local>
2019-01-11 15:38:02 -0200

    Alterado hostname

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const socketListenner = require('./src/server/utils/socketListenner')
 const io = require('socket.io').listen(server)
 const db = require('./src/server/db/connection')
 process.env.MONGOLAB_URI = 'mongodb://test:test123@ds153824.mlab.com:53824/quantocusta'
-global.appHost = 'http://quanto-fica.heroku-app.com'
+global.appHost = 'https://quanto-fica.herokuapp.com'
 global.mongoHost = process.env.MONGOLAB_URI
 
 db.connect(mongoHost)
